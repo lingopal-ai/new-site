e497db47f607413302d6448c4c55c44710e79556
Kyoka-Suigetsu <jeremysamaroo@gmail.com>
2024-01-03 10:39:09 -0400

    Remove actionLabel prop from InfoCard component and whyUs constants

--- assemblyai/app/_shared/components/whyUs/constants.ts
+++ assemblyai/app/_shared/components/whyUs/constants.ts
@@ -4,35 +4,30 @@ const data: {
   title: string;
   Icon: any;
   description: string;
-  actionLabel: string;
 }[] = [
   {
     title: "Easy to integrate + 24/7 support",
     Icon: Icons.EasyIntergrate,
     description:
       "Connect your live stream to us in under 5 minutes.  Our team of AI experts  can help customize our solution for your needs.",
-    actionLabel: "Learn more",
   },
   {
     title: "Captions & Audio",
     Icon: Icons.PricingScales,
     description:
       "Captions and audio are translated in real time in 60+ languages.",
-    actionLabel: "View pricing",
   },
   {
     title: "Cloned Voice",
     Icon: Icons.UptoDate,
     description:
       "Every voice is cloned in real time. Our translated audio sounds identical to the original speakers voice.",
-    actionLabel: "View blog",
   },
   {
     title: "Conversational AI",
     Icon: Icons.CustomerSupport,
     description:
       "Our AI can handle industry vocabulary and is trained to handle how people speak. (Slang, Idioms, and more)",
-    actionLabel: "Join our Discord",
   },
 ];
 
